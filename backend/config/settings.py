2688a6ebc4d761a6b0e2b058a42cf48f85f0ef80
Bharatk003 <bharatkshirsagar003@gmail.com>
2024-07-22 16:23:46 +0530

    add localhost to allowed host

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY =  os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = ["social-nework-for-coders.onrender.com","laocalhost"]
+ALLOWED_HOSTS = ["social-nework-for-coders.onrender.com","localhost"]
 
 # Application definition
 
